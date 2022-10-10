894425c7b8f986691c3ad8fb400191fc7d0575ea
kamilsmr <kamils6035@gmail.com>
2022-10-10 22:16:48 +0300

    dom

---
+++ app.js
@@ -0,0 +1,14 @@
+// var temp = document.querySelector("button")
+// console.log(temp)
+
+// temp.innerText= "hi dear"
+
+// console.log(temp.innerText)
+
+// start
+
+const button = document.querySelector("button");
+
+console.log(button);
+
+button.innerHTML="hello";
